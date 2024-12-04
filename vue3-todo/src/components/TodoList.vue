b842829af2ce67deb128edd7fbfbd86a1a811874
leh87 <ehlee@brainednet.com>
2024-12-04 22:21:38 +0900

    [todo] 할일추가 - 리팩토링(불필요 코드 삭제)

--- vue3-todo/src/components/TodoList.vue
+++ vue3-todo/src/components/TodoList.vue
@@ -5,23 +5,9 @@
 </template>
 
 <script>
-import { ref } from 'vue';
-
 /* eslint-disable */
 export default {
     props: ['todoItems'],
-    setup() {
-        const items = ref([]);
-
-        // function fetchTodos() {
-        //     for (let i = 0; i < localStorage.length; i++) {
-        //         const todoItem = localStorage.key(i);
-        //         items.value.push(todoItem);
-        //     }
-        // }
-
-        return { items }
-    }
 }
 </script>
 
