610e380df1d3850786b2bec9e19edb965e028875
leh87 <ehlee@brainednet.com>
2024-12-08 21:31:41 +0900

    [todo] setup에서 props, context 관련 주의 사항

--- vue3-todo/src/components/TodoList.vue
+++ vue3-todo/src/components/TodoList.vue
@@ -11,9 +11,13 @@
 /* eslint-disable */
 export default {
     props: ['todoItems'],
-    setup(props, context) {
+    // setup(props, context) {
+    // context 경우, {}처럼 사용하는 부분은 디스트럭처링(구조분해할당) 가능
+    // 단, props는 불가. 무조건! props로 사용.
+    setup(props, {emit}) {
         const removeTodo = (item, index) => {
-            context.emit('remove', item, index)
+            // context.emit('remove', item, index)
+            emit('remove', item, index)
         }
 
         return { removeTodo }
