caa0c81f10aa740c466d79cba9b38b3e26655a47
leh87 <ehlee@brainednet.com>
2024-12-20 21:29:18 +0900

    [vue 기본] setup 함수

--- gymcoding/README.md
+++ gymcoding/README.md
@@ -156,3 +156,46 @@
 
   </style>
   ```
+<hr />
+
+### COMPOSITION API
+  ```javascript
+  export default {
+    props: {
+      title: String,
+    },
+
+    // 첫번째 매개변수 => props
+    // 두번째 매개변수 => context
+    setup(props, context) {
+      const counter = ref(0);
+      const increment = () => {
+        counter.value++;
+      };
+
+      console.log(props.title);
+
+      // 속성($attrs와 동일한 비반응형 객체)
+      console.log(context.attrs);
+
+      // 슬롯($slots)에 해당하는 비반응성 객체
+      console.log(context.slots);
+
+      // 이벤트 발생($emit에 해당하는 함수)
+      console.log(context.emit);
+
+      // Public한 속성, 함수를 외부에 노출시에 사용
+      console.log(context.expose);
+
+      return { counter, increment };
+    },
+  };
+  ```
+  - `context` 경우, 구조 분해 할당을 사용 할 수 있다.
+  ```javascript
+  export default {
+    setup(props, { attrs, slots, emit, expose }) {
+      ...
+    }
+  }
+  ```
--- gymcoding/learn-vue3/src/App.vue
+++ gymcoding/learn-vue3/src/App.vue
@@ -1,12 +1,55 @@
 <template>
-	<div>Hello Vue 3</div>
+	<div>{{ counter }}</div>
+	<button @click="increment">click</button>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
-	setup() {
-		return {};
+	props: {
+		title: String,
 	},
+
+	// 첫번째 매개변수 => props
+	// 두번째 매개변수 => context
+	setup(props, context) {
+		const counter = ref(0);
+		const increment = () => {
+			counter.value++;
+		};
+
+		console.log(props.title);
+
+		// 속성($attrs와 동일한 비반응형 객체)
+		console.log(context.attrs);
+
+		// 슬롯($slots)에 해당하는 비반응성 객체
+		console.log(context.slots);
+
+		// 이벤트 발생($emit에 해당하는 함수)
+		console.log(context.emit);
+
+		// Public한 속성, 함수를 외부에 노출시에 사용
+		console.log(context.expose);
+
+		return { counter, increment };
+	},
+
+	// context 구조 분해 할당 가능
+	// setup(props, { attrs, slots, emit, expose }) {
+	// 	// 속성($attrs와 동일한 비반응형 객체)
+	// 	console.log(attrs);
+
+	// 	// 슬롯($slots)에 해당하는 비반응성 객체
+	// 	console.log(slots);
+
+	// 	// 이벤트 발생($emit에 해당하는 함수)
+	// 	console.log(emit);
+
+	// 	// Public한 속성, 함수를 외부에 노출시에 사용
+	// 	console.log(expose);
+	// },
 };
 </script>
 
