4d93a752609917eaf3ef245c6562cfe17081cb33
leh87 <ehlee@brainednet.com>
2025-03-07 01:47:34 +0900

    not found, 중첩 라우트 적용

--- gymcoding/vue3-posts/src/router/index.js
+++ gymcoding/vue3-posts/src/router/index.js
@@ -1,46 +1,78 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
-import AboutView from '@/views/AboutView.vue';
-import PostCreateView from '@/views/posts/PostCreateView.vue';
-import PostDetailView from '@/views/posts/PostDetailView.vue';
-import PostListView from '@/views/posts/PostListView.vue';
-import PostEditView from '@/views/posts/PostEditView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import AboutView from "@/views/AboutView.vue";
+import PostCreateView from "@/views/posts/PostCreateView.vue";
+import PostDetailView from "@/views/posts/PostDetailView.vue";
+import PostListView from "@/views/posts/PostListView.vue";
+import PostEditView from "@/views/posts/PostEditView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
+import NestedView from "@/views/nested/NestedView.vue";
+import NestedOneView from "@/views/nested/NestedOneView.vue";
+import NestedTwoView from "@/views/nested/NestedTwoView.vue";
+import NestedHomeView from "@/views/nested/NestedHomeView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "about",
     component: AboutView,
   },
   {
-    path: '/posts',
-    name: 'PostList',
+    path: "/posts",
+    name: "PostList",
     component: PostListView,
   },
   {
-    path: '/posts/create',
-    name: 'PostCreate',
+    path: "/posts/create",
+    name: "PostCreate",
     component: PostCreateView,
   },
   {
-    path: '/posts/:id',
-    name: 'PostDetail',
+    path: "/posts/:id",
+    name: "PostDetail",
     component: PostDetailView,
   },
   {
-    path: '/posts/:id/edit',
-    name: 'PostEdit',
+    path: "/posts/:id/edit",
+    name: "PostEdit",
     component: PostEditView,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: NotFoundView,
+  },
+  {
+    path: "/nested",
+    name: "Nested",
+    component: NestedView,
+    children: [
+      {
+        path: "",
+        name: "NestedHome",
+        component: NestedHomeView,
+      },
+      {
+        path: "one",
+        name: "NestedOne",
+        component: NestedOneView,
+      },
+      {
+        path: "two",
+        name: "NestedTwo",
+        component: NestedTwoView,
+      },
+    ],
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory('/'),
+  history: createWebHistory("/"),
   routes,
 });
 
